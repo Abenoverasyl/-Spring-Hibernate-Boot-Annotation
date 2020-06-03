6659eb76f3d43ff29dceba2b519bf74a59b0c91f
ErasylAbenov <abenov.erasyl@gmail.com>
2020-06-03 23:44:10 +0600

    Constructor Injection

--- spring-demo-one/src/com/love2code/springdemo/TennisCoach.java
+++ spring-demo-one/src/com/love2code/springdemo/TennisCoach.java
@@ -1,12 +1,39 @@
 package com.love2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
+	// define a default constructor
+//	public TennisCoach() {
+//		System.out.println(">> TennisCoach: inside default constructor");
+//	}
+//	
+//	// define a setter method
+//	public void setFortuneService(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		// TODO Auto-generated constructor stub
+		fortuneService = theFortuneService;
+	}
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice you backhand volley!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 }
