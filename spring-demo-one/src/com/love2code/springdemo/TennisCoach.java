b0e539d81b4b07dad6865370537737f4660a7595
ErasylAbenov <abenov.erasyl@gmail.com>
2020-06-07 10:56:50 +0600

    Bean LifeCycle Method Annotation

--- spring-demo-one/src/com/love2code/springdemo/TennisCoach.java
+++ spring-demo-one/src/com/love2code/springdemo/TennisCoach.java
@@ -1,12 +1,15 @@
 package com.love2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
@@ -16,6 +19,19 @@ public class TennisCoach implements Coach {
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define my init method
+	@PostConstruct
+	public void myStartupStaff() {
+		System.out.println(">> TennisCoach: inside of doMyStartupStaff()");
+	}
+	
+	
+	// define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> Tennis Coach: inside of doMyCleanupStuff()");
 	}
 	
 	/*
