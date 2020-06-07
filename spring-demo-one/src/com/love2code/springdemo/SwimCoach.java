7e716a481d1d310ce3167ff34e2ce49c33ea2d8b
ErasylAbenov <abenov.erasyl@gmail.com>
2020-06-07 16:55:50 +0600

    Defining Spring Beans with Java Code 1

---
+++ spring-demo-one/src/com/love2code/springdemo/SwimCoach.java
@@ -0,0 +1,21 @@
+package com.love2code.springdemo;
+
+public class SwimCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Swim 1000 metrs as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
